d3040485d36948de339e75b9829c52cbec84edf3
simplonboulogne <eeuchin@simplon.co>
2019-11-11 15:06:22 +0100

    add user security

---
+++ src/Entity/UserDetails.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserDetailsRepository")
+ */
+class UserDetails
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime_immutable", nullable=true)
+     */
+    private $lastlogin;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastlogin(): ?\DateTimeImmutable
+    {
+        return $this->lastlogin;
+    }
+
+    public function setLastlogin(?\DateTimeImmutable $lastlogin): self
+    {
+        $this->lastlogin = $lastlogin;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+}
